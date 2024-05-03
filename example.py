a3cbcfff1d0015522b5c458274565d4d7730b3dc
Simon Alibert <alibert.sim@gmail.com>
2024-05-03 14:41:38 +0200

    Update example

--- example.py
+++ example.py
@@ -1,15 +1,20 @@
+import imageio
 import gymnasium as gym
+import numpy as np
 import gym_aloha
 
 env = gym.make("gym_aloha/AlohaInsertion-v0")
 observation, info = env.reset()
+frames = []
 
 for _ in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     image = env.render()
+    frames.append(image)
 
     if terminated or truncated:
         observation, info = env.reset()
 
 env.close()
+imageio.mimsave("example.mp4", np.stack(frames), fps=25)
