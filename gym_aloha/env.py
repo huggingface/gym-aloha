cf92fb149179be2beaca727b23d05205d05f8fa1
danaaubakirova <d.aubakirova@alumni.edu.kz>
2024-09-25 14:49:12 +0200

    fix

--- gym_aloha/env.py
+++ gym_aloha/env.py
@@ -109,7 +109,7 @@ class AlohaEnv(gym.Env):
         # time limit is controlled by StepCounter in env factory
         time_limit = float("inf")
 
-        if task_name  == "transfer_cube":
+        if task_name == "transfer_cube":
             xml_path = ASSETS_DIR / "bimanual_viperx_transfer_cube.xml"
             physics = mujoco.Physics.from_xml_path(str(xml_path))
             task = TransferCubeTask()
