83dd04bfcc61339c9fa9d4eedbce5d157e72b307
yinzi <myinzi123@gmail.com>
2025-02-17 18:03:20 +0800

    create dummy robot branch; use uv to manage package

--- example.py
+++ example.py
@@ -2,12 +2,13 @@ import imageio
 import gymnasium as gym
 import numpy as np
 import gym_aloha
+from tqdm import tqdm
 
 env = gym.make("gym_aloha/AlohaInsertion-v0")
 observation, info = env.reset()
 frames = []
 
-for _ in range(1000):
+for _ in tqdm(range(1000)):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     image = env.render()
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from gym-aloha-dummy!")
+
+
+if __name__ == "__main__":
+    main()
